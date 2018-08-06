6ea0f93e00222fa4e4fe6ed78f6f330d703f435a
Kazumasa Shimomura <kazu.devapp@gmail.com>
2018-08-07 02:15:21 +0900

    project init

---
+++ bch-wallet/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  bch-wallet
+//
+//  Created by 下村一将 on 2018/08/07.
+//  Copyright © 2018 下村一将. All rights reserved.
+//
+
+import UIKit
+import BitcoinCashKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
